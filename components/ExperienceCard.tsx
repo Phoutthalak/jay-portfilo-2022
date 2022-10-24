6a177ab10518b96d459450fedc57f44586372590
Phoutthalak <p.phansalithong@gmail.com>
2022-10-24 09:04:45 +0700

    edit color

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 function ExperienceCard({ experience }: Props) {
     return (
-        <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[375px] md:w-[500px] xl:w-[900px] snap-center bg-[#292929] p-10 
+        <article className="flex flex-col rounded-lg items-center space-y-5 flex-shrink-0 w-[375px] md:w-[500px]  xl:w-[900px] snap-center bg-[#292929] p-10 
         hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
             <motion.img
                 initial={{
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -13,7 +13,7 @@ function WorkExperience({ experiences }: Props) {
             initial={{ opacity: 0, }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className="h-screen flex relative overflow-hidden flex-column text-left  md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
+            className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
             <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">experience</h3>
 
             <div className="w-full flex space-x-5 p-10 overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-black scrollbar-thumb-[#F7AB0A]/80">
